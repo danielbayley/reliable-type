00e5342980f2c32f89397fd86aaa2420ffcd908e
Daniel Bayley <daniel.bayley@me.com>
2025-07-17 12:49:27 +0100

    feat: add initial `type` implementation

---
+++ index.js
@@ -0,0 +1,13 @@
+export default type
+export function type(value) {
+  if (value == null) return String(value)
+  const tag = Reflect.apply(Object.prototype.toString, value, [])
+  const {name} = value.constructor ?? {}
+  switch (name) {
+    case "Number":
+      return Number.isNaN(value) ? "NaN" : Number.isFinite(value) ? name : "Infinity"
+    case "Object":
+    case "": return tag.slice(8, -1)
+    default: return name
+  }
+}
